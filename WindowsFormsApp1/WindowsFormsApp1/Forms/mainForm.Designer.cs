24fbfa2d9ecacfd7bf05e6d8503794d0955e035c
Martin Osman <cppmember95@gmail.com>
2018-10-11 18:28:21 +0200

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Forms/mainForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApp1.Forms
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.lbShow = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(165, 35);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "OpenFile";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // lbShow
+            // 
+            this.lbShow.FormattingEnabled = true;
+            this.lbShow.Location = new System.Drawing.Point(13, 54);
+            this.lbShow.Name = "lbShow";
+            this.lbShow.Size = new System.Drawing.Size(762, 160);
+            this.lbShow.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(233, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OpenFile";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbShow);
+            this.Controls.Add(this.btnOpenFile);
+            this.Name = "mainForm";
+            this.Text = "MiniShell";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.ListBox lbShow;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Forms/mainForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+
+namespace WindowsFormsApp1.Forms
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+            {
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+                if(fbd.ShowDialog()==DialogResult.OK)
+                {
+                    lbShow.Items.Clear();
+                    string[] files = FindFileDir(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+                string[] dirs = Directory.GetFiles(fbd.SelectedPath);
+
+                    foreach (string file in files)
+                    {
+                          lbShow.Items.Add(file);
+
+
+                    }
+                    /*
+                    foreach(string dir in dirs)
+                    {
+                        lbShow.Items.Add(dir);
+                    }*/
+        }
+        }
+        private static string[] FindFileDir(string beginpath)
+        {
+            List<string> findlist = new List<string>();
+
+            /* I begin a recursion, following the order:
+             * - Insert all the files in the current directory with the recursion
+             * - Insert all subdirectories in the list and rebegin the recursion from there until the end
+             */
+            RecurseFind(beginpath, findlist);
+
+            return findlist.ToArray();
+        }
+        private static void RecurseFind(string path, List<string> list)
+        {
+            string[] fl = Directory.GetFiles(path);
+            string[] dl = Directory.GetDirectories(path);
+            if (fl.Length > 0 || dl.Length > 0)
+            {
+                //I begin with the files, and store all of them in the list
+                foreach (string s in fl)
+                    list.Add(s);
+                //I then add the directory and recurse that directory, the process will repeat until there are no more files and directories to recurse
+                foreach (string s in dl)
+                {
+                    list.Add(s);
+                    RecurseFind(s, list);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbShow.Items.Clear();
+        }
+    }
+}
